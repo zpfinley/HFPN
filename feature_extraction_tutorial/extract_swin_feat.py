bc07ff60f0d7d4763ad15409942428e9ec4785bb
zpfinley <33489273+zpfinley@users.noreply.github.com>
2025-12-13 23:07:25 +0800

    Add files via upload

---
+++ feature_extraction_tutorial/extract_swin_feat.py
@@ -0,0 +1,123 @@
+import os
+import timm
+import h5py
+import torch
+import pandas as pd
+import numpy as np
+import glob
+from torchvision.transforms import Compose, Resize, Normalize, ToTensor
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+
+class AVEDataset(Dataset):
+    def __init__(self, data_root, split='train'):
+        super(AVEDataset, self).__init__()
+        self.split = split
+        self.sample_order_path = os.path.join(data_root, f'{split}_order.h5')
+        self.raw_gt = pd.read_csv(data_root+"/Annotations.txt", sep="&", header=None)
+        self.video_folder = "/AVE_Dataset/video_frames"
+        self.my_normalize = Compose([Resize([192, 192],
+                                    interpolation=Image.BICUBIC),
+                                     ToTensor(),
+                                     Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)])
+        self.h5_isOpen = False
+
+
+    def __getitem__(self, index):
+        if not self.h5_isOpen:
+
+            self.sample_order = h5py.File(self.sample_order_path, 'r')['order']
+            self.h5_isOpen = True
+
+
+        sample_index = self.sample_order[index]
+        file_name = self.raw_gt.iloc[sample_index][1]
+
+        total_img = []
+        for vis_idx in range(10):
+            img_path = self.video_folder + '/' + file_name + '/' + str("{:04d}".format(vis_idx)) + '.jpg'
+            tmp_img = Image.open(img_path).convert('RGB')
+            tmp_img = self.my_normalize(tmp_img)
+            total_img.append(tmp_img)
+        total_img = torch.stack(total_img)
+
+        return total_img, file_name
+
+
+    def __len__(self):
+        f = h5py.File(self.sample_order_path, 'r')
+        sample_num = len(f['order'])
+        f.close()
+        return sample_num
+
+if __name__ == '__main__':
+
+    model = timm.create_model('swinv2_large_window12_192_22k', pretrained=True)
+    model = model.cuda()
+    model = model.eval()
+
+    train_dataloader = DataLoader(
+            AVEDataset('/AVE_data/', split='train'), # split= val, train, test
+            batch_size=1,
+            shuffle=False,
+            num_workers=1,
+            pin_memory=False
+        )
+    print(len(train_dataloader))
+
+    for n_iter, batch_data in enumerate(train_dataloader):
+        print("***************", n_iter)
+
+        visual_feat, video_name = batch_data
+
+        visual_feat = visual_feat.cuda()
+        bz, t, c, h, w = visual_feat.shape
+        visual_feat = visual_feat.view(bz*t, c, h, w)
+
+        with torch.no_grad():
+
+            s2_save_dir = "/AVE_data/swinv2_large_feat_s2"
+            s3_save_dir = "/AVE_data/swinv2_large_feat_s3"
+            s4_save_dir = "/AVE_data/swinv2_large_feat_s4"
+
+            s2_outfile = os.path.join(s2_save_dir, video_name[0] + '.npy')
+            s3_outfile = os.path.join(s3_save_dir, video_name[0] + '.npy')
+            s4_outfile = os.path.join(s4_save_dir, video_name[0] + '.npy')
+
+            x = model.patch_embed(visual_feat)
+            if model.absolute_pos_embed is not None:
+                x = x + model.absolute_pos_embed
+            x = model.pos_drop(x)
+
+            nums_x = []
+            for layer in model.layers:
+                for blk in layer.blocks:
+                    x = x + blk.drop_path1(blk.norm1(blk._attn(x)))
+                    x = x + blk.drop_path2(blk.norm2(blk.mlp(x)))
+                nums_x.append(x)
+                x = layer.downsample(x)
+
+            if os.path.exists(s2_outfile):
+                print("文件已经存在")
+            else:
+                s2_image_features = nums_x[1].cpu().numpy()
+                np.save(s2_outfile, s2_image_features)
+
+            if os.path.exists(s3_outfile):
+                print("文件已经存在")
+            else:
+                s3_image_features = nums_x[2].cpu().numpy()
+                np.save(s3_outfile, s3_image_features)
+
+            if os.path.exists(s4_outfile):
+                print("文件已经存在")
+            else:
+                s4_image_features = nums_x[3].cpu().numpy()
+                np.save(s4_outfile, s4_image_features)
+
+
+
+
+
+
